eb4afa46503b8bd9eb9368f8480cd54e21cd6d0c
pavel.datunashvili <pavel.datunashvili@cinimex.ru>
2019-02-07 17:43:41 +0300

    let's start work

---
+++ src/main/java/ge/vazisu/winbet/WinbetApplication.java
@@ -0,0 +1,14 @@
+package ge.vazisu.winbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinbetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinbetApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/ge/vazisu/winbet/WinbetApplicationTests.java
@@ -0,0 +1,17 @@
+package ge.vazisu.winbet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WinbetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
